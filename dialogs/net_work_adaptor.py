58927027f01998ee285b6b854ad84095ff988e59
dcmotocorp <dipenpatell2496.com>
2024-04-12 20:08:01 +0530

    sdf

--- dialogs/net_work_adaptor.py
+++ dialogs/net_work_adaptor.py
@@ -57,7 +57,7 @@ class NetworkAdaptorScreen:
         # Add labels to popup_bottom_win
         for index, label in enumerate(self.labels):
             color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
-            self.auth_bottom_win.addstr( 2+ index, 4, "[]", color_pair)
+            self.auth_bottom_win.addstr( 2+ index, 2, "[]", color_pair)
             self.auth_bottom_win.addstr( 2+ index, 5, label[0], color_pair)
             self.auth_bottom_win.addstr( 2+ index, 15, label[1], color_pair)
             self.auth_bottom_win.addstr( 2+ index, 35, label[2], color_pair)
@@ -96,7 +96,7 @@ class NetworkAdaptorScreen:
       
             for index, label in enumerate(self.labels):
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
-                self.auth_bottom_win.addstr( 2+ index, 4, "[]", color_pair)
+                self.auth_bottom_win.addstr( 2+ index, 2, "[]", color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 5, label[0], color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 15, label[1], color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 35, label[2], color_pair)
@@ -111,7 +111,7 @@ class NetworkAdaptorScreen:
            
             for index, label in enumerate(self.labels):
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
-                self.auth_bottom_win.addstr( 2+ index, 4, "[]", color_pair)
+                self.auth_bottom_win.addstr( 2+ index, 2, "[]", color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 5, label[0], color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 15, label[1], color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 35, label[2], color_pair)
@@ -124,9 +124,9 @@ class NetworkAdaptorScreen:
             
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
                 if index == self.current_selected_label_index :
-                    self.auth_bottom_win.addstr( 2+ index, 4, "[0]", color_pair)
+                    self.auth_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
                 else:
-                    self.auth_bottom_win.addstr( 2+ index, 4, "[]", color_pair)
+                    self.auth_bottom_win.addstr( 2+ index, 2, "[]", color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 5, label[0], color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 15, label[1], color_pair)
                 self.auth_bottom_win.addstr( 2+ index, 35, label[2], color_pair)
