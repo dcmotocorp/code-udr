4c30c87dc0a115bbc5c2d6dfa18ab725df9b400e
dcmotocorp <dipenpatell2496.com>
2024-04-12 20:05:29 +0530

    sldkjfl

--- dialogs/net_work_adaptor.py
+++ dialogs/net_work_adaptor.py
@@ -118,11 +118,12 @@ class NetworkAdaptorScreen:
             self.auth_bottom_win.refresh()
 
         if key.name == "space":
-        
+            
+            self.current_selected_label_index = self.selected_index
             for index, label in enumerate(self.labels):
             
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
-                if index == self.selected_index :
+                if index == self.current_selected_label_index :
                     self.auth_bottom_win.addstr( 2+ index, 4, "[0]", color_pair)
                 else:
                     self.auth_bottom_win.addstr( 2+ index, 4, "[]", color_pair)
