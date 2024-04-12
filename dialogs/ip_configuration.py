e9c03d61ec13d81357f2f49e07d1ffa0e492f07c
dcmotocorp <dipenpatell2496.com>
2024-04-13 00:45:53 +0530

    kjansd

--- dialogs/ip_configuration.py
+++ dialogs/ip_configuration.py
@@ -99,7 +99,11 @@ class IPConfigurationScreen:
                  self.selected_index = 0
             for index, label in enumerate(self.labels):
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
-                self.auth_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
+                if self.current_seleected_parameter == index:
+                    self.auth_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
+                else:
+                    self.auth_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
+
                 self.auth_bottom_win.addstr(2 + index, 5, label, color_pair)
             self.auth_bottom_win.refresh()
             self.setup_network_adaptor_screen() 
@@ -111,7 +115,11 @@ class IPConfigurationScreen:
                  self.selected_index = 1
             for index, label in enumerate(self.labels):
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
-                self.auth_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
+                if self.current_seleected_parameter == index:
+                    self.auth_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
+                else:
+                    self.auth_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
+
                 self.auth_bottom_win.addstr(2 + index, 5, label, color_pair)
             self.auth_bottom_win.refresh()
             self.setup_network_adaptor_screen()
