1bd65e73bfbb474a70d9c7f9bceab483eabf7ecc
dcmotocorp <dipenpatell2496.com>
2024-04-13 21:47:39 +0530

    hrh

--- dialogs/ip_configuration.py
+++ dialogs/ip_configuration.py
@@ -143,6 +143,8 @@ class IPConfigurationScreen:
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
                 if self.current_seleected_parameter == index:
                     self.auth_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
+                    if self.current_seleected_parameter ==1:
+                        self.set_up_in_address_field()
                 else:
                     self.auth_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
 
@@ -159,6 +161,8 @@ class IPConfigurationScreen:
                 color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
                 if self.current_seleected_parameter == index:
                     self.auth_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
+                    if self.current_seleected_parameter ==1:
+                        self.set_up_in_address_field()
                 else:
                     self.auth_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
 
