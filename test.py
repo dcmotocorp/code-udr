6ae81bb1c60242bd6f4744403dd6a1af24cf3f8b
dcmotocorp <dipenpatell2496.com>
2024-04-13 16:24:33 +0530

    lksjfdkmsd

--- test.py
+++ test.py
@@ -412,14 +412,16 @@ class NovaiguApplication:
                         try:
                             selected_value = self.ssh_screen.current_label_head
                             self.logger_.log_info("ssh screen username {} selected_value {}".format(self.username_input,selected_value))
-                            if selected_value:
-                                if selected_value == 0:
-                                    
-                                    self.user_data_base.update_user_settings(self.username_input,ssh_enable=True)
-                                    self.system_controller.enable_ssh()
-                                else:
-                                    self.user_data_base.update_user_settings(self.username_input,ssh_enable=False)
-                                    self.system_controller.disable_ssh()    
+
+                            if selected_value == 0:    
+                                self.logger_.log_info("inside 0 ssh screen username {} selected_value {}".format(self.username_input,selected_value))                                
+                                self.user_data_base.update_user_settings(self.username_input,ssh_enable=True)
+                                self.system_controller.enable_ssh()
+                            elif selected_value == 1:
+                                self.logger_.log_info("inside 1 ssh screen username {} selected_value {}".format(self.username_input,selected_value))
+                                self.user_data_base.update_user_settings(self.username_input,ssh_enable=False)
+                                self.system_controller.disable_ssh() 
+
                             self.system_config.active_status = True
                             self.system_config.update_password_screen = False 
                             self.ssh_screen.clear()
@@ -437,15 +439,15 @@ class NovaiguApplication:
                 elif current_screen == LOCK_DOWN_MODE:
                     if   hasattr(self, 'lock_down_screen')  and self.lock_down_screen !=None  and self.lock_down_screen.update_status == True  :
                         selected_value = self.lock_down_screen.current_label_head
-                        if selected_value:
-                            if selected_value == 0:
-                                self.logger_.log_info("lock down screen username {}".format(self.username_input))
-                                self.user_data_base.update_user_settings(self.username_input,is_lockdown=True)
-                                self.system_controller.enable_lockdown_mode()
-                            else:
-                                self.logger_.log_info("lock down screen username else {}".format(self.username_input))
-                                self.user_data_base.update_user_settings(self.username_input,is_lockdown=False)
-                                self.system_controller.exit_lockdown_mode()
+                        
+                        if selected_value == 0:
+                            self.logger_.log_info("lock down screen username {}".format(self.username_input))
+                            self.user_data_base.update_user_settings(self.username_input,is_lockdown=True)
+                            self.system_controller.enable_lockdown_mode()
+                        elif selected_value == 1:
+                            self.logger_.log_info("lock down screen username else {}".format(self.username_input))
+                            self.user_data_base.update_user_settings(self.username_input,is_lockdown=False)
+                            self.system_controller.exit_lockdown_mode()
                         self.system_config.active_status = True
                         self.system_config.update_password_screen = False 
                         self.lock_down_screen.clear()
