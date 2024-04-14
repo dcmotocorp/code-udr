d84ddd25f8688dab8f763596c3dda61a947f463f
dcmotocorp <dipenpatell2496.com>
2024-04-14 10:08:35 +0530

    lkasjd

--- dialogs/ip_configuration.py
+++ dialogs/ip_configuration.py
@@ -84,39 +84,56 @@ class IPConfigurationScreen:
 
     
     def setup_network_adaptor_screen(self):
-        auth_screen_height = 15
-        auth_screen_width = 50
+        auth_screen_height = 10
+        auth_screen_width = 40
         popup_y = (self.screen_height - auth_screen_height // 2) // 2
         popup_x = (self.screen_width - auth_screen_width) // 2
-        
+        self.hostname_screen = curses.newwin(auth_screen_height, auth_screen_width, popup_y, popup_x)
 
         # Calculate dimensions for the two partitions within the pop-up window
         popup_top_height = max(int(0.3 * auth_screen_height), 1)
         popup_bottom_height = auth_screen_height - popup_top_height
-        
-        self.hostname_screen = curses.newwin(auth_screen_height, auth_screen_width, popup_y, popup_x)
 
-        self.auth_top_win = self.hostname_screen.subwin(popup_top_height, auth_screen_width, popup_y, popup_x)
-        self.auth_bottom_win = self.hostname_screen.subwin(popup_bottom_height, auth_screen_width,
+        # Create windows for each partition within the pop-up window
+        auth_top_win = self.hostname_screen.subwin(popup_top_height, auth_screen_width, popup_y, popup_x)
+        auth_bottom_win = self.hostname_screen.subwin(popup_bottom_height, auth_screen_width,
                                                              popup_y + popup_top_height, popup_x)
 
         # Set background colors for each partition within the pop-up window
-        self.auth_top_win.bkgd(' ', curses.color_pair(1))  # Yellow background
-        self.auth_bottom_win.bkgd(' ', curses.color_pair(2))  # Grey background
-        
-        self.in_config = curses.newwin(1, 20, popup_y + popup_top_height, popup_x+10)
-        self.in_config.refresh()
+        auth_top_win.bkgd(' ', curses.color_pair(1))  # Yellow background
+        auth_bottom_win.bkgd(' ', curses.color_pair(2))  # Grey background
+
+        # Add label to auth_top_win
+        label_text = "Configure Hostname"
+        label_x = (auth_screen_width - len(label_text)) // 2
+        label_y = (popup_top_height - 1) // 2  # Center vertically
+        auth_top_win.addstr(label_y, label_x, label_text, curses.color_pair(4))
+
+        username_label = "Hostname :  ["
+        auth_bottom_win.addstr(1, 1, username_label, curses.color_pair(3))
+
+        end_bracket_user = "]"
+        auth_bottom_win.addstr(1, 35, end_bracket_user, curses.color_pair(3))
+
+        # Add label to popup_bottom_win
+        label_text_bottom_esc = "<Esc> Cancel"
+        auth_bottom_win.addstr(5, 25, label_text_bottom_esc, curses.color_pair(3))
+
+        label_text_bottom_enter_ok = "<Enter> Ok"
+        auth_bottom_win.addstr(5, 11, label_text_bottom_enter_ok, curses.color_pair(3))
+
+        # Create username input box
+        user_input_y = popup_y + popup_top_height + 1
+        user_input_x = popup_x + 15
+        self.current_password_win = curses.newwin(1, 20, user_input_y, user_input_x)
+        self.current_password_win.refresh()
         
 
-        self.in_config.addstr(0, 10, "test", curses.color_pair(1))
-        self.in_config.refresh()
-
-
-        
-        self.auth_top_win.refresh()
-        self.auth_bottom_win.refresh()
-        
         curses.curs_set(1)
+        self.hostname_screen.refresh()
+        self.current_hostname = self.system_controller.get_hostname()
+        self.current_password_win.addstr(0, 0, self.current_hostname, curses.color_pair(2))
+        self.current_password_win.refresh()
     
         
 
