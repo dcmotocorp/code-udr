24890a5af07f67c2daa2fcde4c778afe880fa344
dcmotocorp <dipenpatell2496.com>
2024-04-14 11:13:43 +0530

    wrew

--- dialogs/ip_configuration.py
+++ dialogs/ip_configuration.py
@@ -96,7 +96,7 @@ class IPConfigurationScreen:
         
         user_input_y = popup_y + popup_top_height + 5
         user_input_x = popup_x + 30
-        self.in_address_change = curses.newwin(1, len(self.ip_address)+1, user_input_y, user_input_x)
+        self.in_address_change = curses.newwin(1, 16, user_input_y, user_input_x)
         self.in_address_change.refresh()
     
         self.in_address_change.addstr(0, 0, self.ip_address, curses.color_pair(2))
