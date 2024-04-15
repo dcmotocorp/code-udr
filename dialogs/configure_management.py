17e2d57ef7922f688a0933ca4d4ecd594c6b93dc
dcmotocorp <dipenpatell2496.com>
2024-04-15 10:36:39 +0530

    asd

--- dialogs/configure_management.py
+++ dialogs/configure_management.py
@@ -24,8 +24,8 @@ class ConfigureManagement:
         self.label_count = len(self.labels)
         self.selected_color_pair = curses.color_pair(5)  # Color pair for selected label
         self.normal_color_pair = curses.color_pair(4)    # Color pair for normal label
-        self.sys_config = {NETWORK_ADAPTOR:["Aapters","Explore connected networks effortlessly through the Network Adapter's",""],
-                            IP_CONFIGURATION: ["IP Configuration","Modify and update IP addresses for the management interface.","Ensure secure and efficient network management by customizing your \nIP settings"] ,
+        self.sys_config = {NETWORK_ADAPTOR:["Aapters","Explore connected networks effortlessly","through the Network Adapter's"],
+                            IP_CONFIGURATION: ["IP Configuration","Modify and update IP addresses for the management .","interface Ensure secure and efficient network management\n by customizing your IP settings ",] ,
                             DNS_SERVER:["DNS Service","Efficiently configure and select from available DNS","servers to optimize network performance and connectivity"] 
                          }
         
