bf175a5a20f4ebbd72af943c3aa01c06be606af2
dcmotocorp <dipenpatell2496.com>
2024-04-16 16:28:48 +0530

    test

--- dialogs/ip_configuration.py
+++ dialogs/ip_configuration.py
@@ -171,26 +171,25 @@ class IPConfigurationScreen:
 
         
          # Add labels to popup_bottom_win
-        if self.current_seleected_parameter and self.starting_state ==True :
-                self.starting_state =False 
+        if not self.current_selected_label_index:
+                
                 
                 for index, label in enumerate(self.labels):
-                # color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
+                    color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
                     if self.current_seleected_parameter == index:
-                            self.ip_bottom_win.addstr( 2+ index, 2, "[0]",  self.selected_color_pair)
-                            self.ip_bottom_win.addstr( 2+ index, 5, label, self.selected_color_pair)
+                            self.ip_bottom_win.addstr( 2+ index, 2, "[0]",  color_pair)
+                            self.ip_bottom_win.addstr( 2+ index, 5, label, color_pair)
                     else:
-                        self.ip_bottom_win.addstr( 2+ index, 2, "[ ]", self.normal_color_pair)
-                        self.ip_bottom_win.addstr( 2+ index, 5, label, self.normal_color_pair)
-
-                
-                    
+                        self.ip_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
+                        self.ip_bottom_win.addstr( 2+ index, 5, label, color_pair)
 
         else:     
             for index, label in enumerate(self.labels):
-                color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
-                if self.current_seleected_parameter == index:
-                        self.ip_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
+                # color_pair = self.selected_color_pair if index == self.selected_index else self.normal_color_pair
+                if self.current_seleected_parameter == index and self.starting_state == True:
+                    self.starting_state = False 
+                    color_pair = self.selected_color_pair
+                    self.ip_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
                 else:
                     self.ip_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
 
@@ -276,8 +275,6 @@ class IPConfigurationScreen:
                     self.ip_bottom_win.addstr( 2+ index, 2, "[0]", color_pair)
                 else:
                     self.ip_bottom_win.addstr( 2+ index, 2, "[ ]", color_pair)
-                
- 
 
                 self.ip_bottom_win.addstr(2 + index, 5, label, color_pair)
             self.ip_bottom_win.refresh()
